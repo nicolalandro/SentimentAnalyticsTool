b54861d75e2fe78a27a4ba609b5d544812fc1927
nicolalandro <nicolaxx94@live.it>
2017-06-15 20:06:55 +0200

    PieChart

--- src/main/scala/HomePresenter.scala
+++ src/main/scala/HomePresenter.scala
@@ -1,25 +1,42 @@
+
+
+import javafx.collections.FXCollections
+import javafx.scene.chart.PieChart.Data
+
 import scalafx.event.ActionEvent
-import scalafx.scene.chart.AreaChart
+import scalafx.scene.chart.PieChart
 import scalafxml.core.macros.sfxml
 
 @sfxml
-class HomePresenter(mAreaChart:AreaChart[Number,Number]) {
+class HomePresenter(mPieChart:PieChart) {
 
-  mAreaChart.setTitle("Iteration Mood")
+  mPieChart.setTitle("Iteration Mood")
+
+  var mObservableList = FXCollections.observableArrayList[Data];
+  mObservableList.add(new Data("0",0))
+  mObservableList.add(new Data("1",0))
+  mObservableList.add(new Data("2",0))
+  mObservableList.add(new Data("3",0))
+  mObservableList.add(new Data("4",0))
+  mPieChart.setData(mObservableList)
 
   def moodFace0(event: ActionEvent) = {
-    println("yeeeee")
+    addDataToPie(0,1)
   }
   def moodFace1(event: ActionEvent) = {
-    println("yeeeee")
+    addDataToPie(1,1)
   }
   def moodFace2(event: ActionEvent) = {
-    println("yeeeee")
+    addDataToPie(2,1)
   }
   def moodFace3(event: ActionEvent) = {
-    println("yeeeee")
+    addDataToPie(3,1)
   }
   def moodFace4(event: ActionEvent) = {
-    println("yeeeee")
+    addDataToPie(4,1)
+  }
+
+  def addDataToPie(x:Int, y:Int) ={
+    mObservableList.get(x).setPieValue(mObservableList.get(x).getPieValue+1)
   }
 }
