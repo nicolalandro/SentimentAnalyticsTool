0a74b10358d4381fe6c40e08c278c76c12858dac
nicolalandro <nicolaxx94@live.it>
2017-07-08 19:37:23 +0200

    test con assert

--- src/test/scala/HomePageTest.scala
+++ src/test/scala/HomePageTest.scala
@@ -1,22 +1,34 @@
+import java.io.File
 import javafx.scene.layout.HBox
 
-import org.scalatest.{FlatSpec, Matchers}
-
+import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
+import javafx.scene.chart.PieChart
 import javafx.scene.control.Button
+import javax.xml.bind.{JAXBContext, Marshaller}
 
-class HomePageTest extends FlatSpec with Matchers {
+class HomePageTest extends FlatSpec with Matchers with BeforeAndAfterEach {
+  var m = MainObject
 
-  it should "start main" in {
-    var m = MainObject
-
+  override protected def beforeEach(): Unit = {
     new Thread(new Runnable {
       override def run(): Unit = m.main(Array())
     }).start()
 
     Thread.sleep(4000)
+  }
 
+  override protected def afterEach(): Unit = {
+    var file:File = new File("data.xml")
+    var jaxbContext = JAXBContext.newInstance(classOf[SentimentIterationData])
+    var jaxbMarshaller = jaxbContext.createMarshaller
+    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
+    jaxbMarshaller.marshal(new SentimentIterationData, file)
+  }
+
+
+  it should "start main" in {
     val label = m.stage.getScene.getRoot.getChildrenUnmodifiable.get(0)
-    val chart = m.stage.getScene.getRoot.getChildrenUnmodifiable.get(2)
+    val chart = m.stage.getScene.getRoot.getChildrenUnmodifiable.get(2).asInstanceOf[PieChart]
 
     val hbox = m.stage.getScene.getRoot.getChildrenUnmodifiable.get(1).asInstanceOf[HBox]
 
@@ -25,6 +37,8 @@ class HomePageTest extends FlatSpec with Matchers {
     button0.fire()
 
     Thread.sleep(4000)
+
+    chart.getData.get(0).getPieValue should be(1.0)
   }
 
 }
