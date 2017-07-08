014e1efc8feb172040cc795dc62de9a6eaebd305
nicolalandro <nicolaxx94@live.it>
2017-07-08 09:32:23 +0200

    Sistemato FXML

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,12 +1,28 @@
+import java.io.File
+import java.net.URL
+import java.nio.file.Paths
+
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
 import scalafxml.core.{FXMLView, NoDependencyResolver}
 
+
 object main extends JFXApp {
   stage = new PrimaryStage() {
     title = "Home"
-    scene = new Scene(FXMLView(getClass.getClassLoader.getResource("home.fxml"), NoDependencyResolver))
+    scene = new Scene(FXMLView(getFXMLFromJarFolder("home.fxml"), NoDependencyResolver))
+  }
+  println(getClass.getProtectionDomain.getCodeSource.getLocation.toURI.getPath)
+
+  def getFXMLFromJarFolder(s: String): URL = {
+//    new URL("file://"+getClass.getProtectionDomain.getCodeSource.getLocation.toURI.getPath + s)
+    val absolutePath:String = Paths.get(getClass.getProtectionDomain.getCodeSource.getLocation.toURI.getPath).toString
+    val file = new File(absolutePath)
+    val path = file.getParentFile.getAbsolutePath
+    println(absolutePath)
+    println(path)
+    new URL("file://"+path+"/FXML/"+s)
   }
 }
